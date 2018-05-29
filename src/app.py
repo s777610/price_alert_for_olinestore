8e576764d08ee9a7c169e1b18074a521e8a18099
WeiChengHung <s777610@gmail.com>
2018-05-28 19:16:12 -0700

    add some link

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from src.common.database import Database
 
@@ -18,6 +18,10 @@ we need a secret key
 @app.before_first_request
 def init_db():
     Database.initialize()
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 from src.models.users.views import user_blueprint
 app.register_blueprint(user_blueprint, url_prefix="/users")
