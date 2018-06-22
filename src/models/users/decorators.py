edfc884b6dc2ed1f027a56af1cb3fe68595719cf
WeiChengHung <s777610@gmail.com>
2018-06-22 15:20:50 -0700

    Adding requires admin permissions decorators

--- src/models/users/decorators.py
+++ src/models/users/decorators.py
@@ -1,6 +1,8 @@
 from functools import wraps
+from src.app import app
 
 from flask import session, url_for, redirect, request
+
 
 
 def requires_login(func):
@@ -8,5 +10,15 @@ def requires_login(func):
     def decorated_function(*args, **kwargs):
         if 'email' not in session.keys() or session['email'] is None:
             return redirect(url_for('users.login_user', next=request.path))  # after login, go to next
+        return func(*args, **kwargs)
+    return decorated_function
+
+def requires_admin_permissions(func):
+    @wraps(func)  # wrap the func with decorated_function
+    def decorated_function(*args, **kwargs):
+        if 'email' not in session.keys() or session['email'] is None:
+            return redirect(url_for('users.login_user', next=request.path))  # after login, go to next
+        if session['email'] not in app.config.ADMINS:
+            return redirect(url_for('users.login_user'))
         return func(*args, **kwargs)
     return decorated_function
